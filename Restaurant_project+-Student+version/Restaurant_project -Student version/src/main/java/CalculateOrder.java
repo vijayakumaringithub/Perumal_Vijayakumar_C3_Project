bf5efe6454bfcdd88547fb8a0d46f4af84f702d5
vijayakumaringithub <vijayakumar.perumal.in@gmail.com>
2022-06-08 22:43:23 +0530

    Part 3: Solution

---
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/CalculateOrder.java
@@ -0,0 +1,12 @@
+import java.util.List;
+public class CalculateOrder {
+    private Restaurant restaurant;
+    private List<Item> items;
+
+    public CalculateOrder(Restaurant restaurant)
+    {
+        this.restaurant = restaurant;
+    }
+
+
+}
